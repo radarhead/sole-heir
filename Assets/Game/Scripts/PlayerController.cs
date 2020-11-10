fb3f96adbc74004f10d581800aa5309592e47ea7
Tim Callies <timcallies@gmail.com>
2020-11-10 16:57:08 -0600

    Basic Networking

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace SoleHeir
+{
+    public class PlayerController : NetworkBehaviour
+    {
+        private float speed = 10;
+        private float smoothTime = 0.05f;
+        private Vector3 acceleration = Vector3.zero;
+        Rigidbody2D body;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Only process inputs for local player.
+            if (isLocalPlayer)
+            {
+                float h = Input.GetAxisRaw("moveX");
+                float v = Input.GetAxisRaw("moveY");
+
+                var newVelocity = new Vector3(h, v, 0);
+                newVelocity.Normalize();
+                //newVelocity.Magnitude(speed);
+                body.velocity = Vector3.SmoothDamp(body.velocity, newVelocity*speed, ref acceleration, smoothTime);
+            }
+        }
+    }
+}
+
